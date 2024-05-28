d29761a6ff76664ed0563189f0ac4136cd42ae1b
Wallef Ryan <146087764+WallefRyan@users.noreply.github.com>
2024-05-28 15:26:31 -0300

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -14,6 +14,11 @@ ajusteTamanhoPalcoJogo()
 
 
 function posicaoRandomica(){
+
+    //Remove mosquito anterior (caso ele exista)
+    if (document.getElementById('mosquito')) {
+        document.getElementById('mosquito').remove()
+    }
     //Fazer o Mosquito se mover de forma randomico (aleatorio) em conforme com o ajuste de palco sem estourar a pagina
     var posicaoX = Math.floor(Math.random() * largura) - 90
     var posicaoY = Math.floor(Math.random() * altura) - 90
@@ -27,20 +32,30 @@ function posicaoRandomica(){
     //Cria o elemento HTML de forma dinamica e incluindo esse elemento no Body da Pagina
     var mosquito = document.createElement('img')
     mosquito.src = 'src/img/mosca.png'
-    mosquito.className = tamanhoAleatorio()
+    mosquito.className = tamanhoAleatorio() + ' ' +ladoAleatorio()
     // forma a cordenada em pixel
     mosquito.style.left = posicaoX + 'px'
     mosquito.style.top = posicaoY + 'px'
     mosquito.style.position = 'absolute'
+    
+    //cria o lemento ID para a imagem
+    mosquito.id ='mosquito'
+    //Adiciona o evento de clique na imagem
+    mosquito.onclick = function() {
+        this.remove()
+    }
+    
+    
+
 
     document.body.appendChild(mosquito)
 
 }
 
+
 function tamanhoAleatorio() {
-    //Cria 3 classes de 0 a 2 para ajustar-mos o tamanho
+    //Cria 3 classes de 0 a 2 para ajustar-mos o tamanho 
     var classe = Math.floor(Math.random() * 3 )
-    console.log(classe)
 
     switch(classe){
         case 0:
@@ -49,6 +64,19 @@ function tamanhoAleatorio() {
             return "mosquito2"
         case 2:
             return "mosquito3"
-        default:
     }
+}
+
+function ladoAleatorio(){
+   //Muda o lado direiro e esquedo da imagem aleatoriamente
+    var classe = Math.floor(Math.random() * 2 )
+    
+    switch(classe){
+        case 0:
+            return "ladoA"
+        case 1:
+            return "ladoB"
+
+    }
+
 }
